c9f5abf26c3aca0655f8a22038472460648a2eec
FranceMpholo <mpholofrance1@outlook.com>
2022-10-14 17:19:37 -0700

    <= added

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -9,9 +9,9 @@ int main(void)
 {
 	int alph;
 
-	for (alph = 'a'; alph < 'z'; alph++)
+	for (alph = 'a'; alph <= 'z'; alph++)
 		putchar(alph);
-	for (alph = 'A'; alph < 'Z'; alph++)
+	for (alph = 'A'; alph <= 'Z'; alph++)
 		putchar(alph);
 	printf("\n");
 	return (0);
