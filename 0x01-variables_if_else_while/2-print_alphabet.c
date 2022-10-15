bc46c91912213f454021232ceb8081551364fef1
FranceMpholo <mpholofrance1@outlook.com>
2022-10-15 05:12:19 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,9 +12,7 @@ int main(void)
 	for (alph = 'a'; alph <= 'z'; alph++)
 	{
 		putchar(alph);
-		putchar('\n');
 	}
-	for (alph = 'A'; alph <= 'Z'; alph++)
-		putchar(alph);
+	putchar('\n');
 	return (0);
 }
