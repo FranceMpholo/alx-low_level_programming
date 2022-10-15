9661154b6cea0db3bf99ad050a21d51fe03299d1
FranceMpholo <mpholofrance1@outlook.com>
2022-10-15 06:04:05 -0700

    ASCII CODE FOR NUMBERS

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Funtion
+ *
+ ** Return: Always 0 successful
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	putchar('\n');
+	return (0);
+}
