a2094f1259686e0fb72768008e2496cb34360bf4
FranceMpholo <mpholofrance1@outlook.com>
2022-10-15 05:06:56 -0700

    braces for loop

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,8 +10,10 @@ int main(void)
 	char alph;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
+	{
 		putchar(alph);
 		putchar('\n');
+	}
 	for (alph = 'A'; alph <= 'Z'; alph++)
 		putchar(alph);
 	return (0);
