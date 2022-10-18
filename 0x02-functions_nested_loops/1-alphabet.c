215df93136f125ca4c04710b3539e0d2d14e60ec
FranceMpholo <mpholofrance1@outlook.com>
2022-10-18 13:07:40 -0700

    Spacing warnig betty

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main -Function
+ *
+ * Return: Always 0 Success
+ */
+int main(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
