4a101eacd96ffbb27b38febc983f64e68c096e7c
FranceMpholo <mpholofrance1@outlook.com>
2022-10-18 13:19:07 -0700

    comment style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
+
 /**
- * main -Function
- *
- * Return: Always 0 Success
+ * print_alphabet - in lower case
  */
 void print_alphabet(void)
 {
@@ -15,5 +14,6 @@ void print_alphabet(void)
 		_putchar(ch);
 		ch++;
 	}
+
 	_putchar('\n');
 }
