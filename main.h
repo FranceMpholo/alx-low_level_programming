9b065e73da54a82c87a54e4cc358509c26e6aaf3
FranceMpholo <113932996+FranceMpholo@users.noreply.github.com>
2023-01-23 08:32:00 +0200

    Bit Manupilation

--- main.h
+++ main.h
@@ -1,13 +1,13 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
-char *_strcat(char *dest, char *src);
-har *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *leet(char *);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
